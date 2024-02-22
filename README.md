aa498c56ec1ebf964eb52825b5573bb885aed162
hassaneljebyly <hassaneljeby@gmail.com>
2024-02-22 22:45:20 +0100

    updates

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 # Frontend Mentor - Multi-step form solution
 
-This is a solution to the [Multi-step form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/multistep-form-YVAnSdqQBJ). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Multi-step form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/multistep-form-YVAnSdqQBJ). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
 
 ## Table of contents
 
-- [Overview](#overview)
-  - [The challenge](#the-challenge)
-  - [Screenshot](#screenshot)
-  - [Links](#links)
-- [My process](#my-process)
-  - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
-  - [Continued development](#continued-development)
-  - [Useful resources](#useful-resources)
-- [Author](#author)
-- [Acknowledgments](#acknowledgments)
+- [Frontend Mentor - Multi-step form solution](#frontend-mentor---multi-step-form-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+    - [Screenshot](#screenshot)
+    - [Links](#links)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [What I learned](#what-i-learned)
+    - [Continued development](#continued-development)
+    - [Useful resources](#useful-resources)
+  - [Author](#author)
+  - [Acknowledgments](#acknowledgments)
 
 **Note: Delete this note and update the table of contents based on what sections you keep.**
 
@@ -38,14 +40,6 @@ Users should be able to:
 
 ![](./screenshot.jpg)
 
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
-
 ### Links
 
 - Solution URL: [Add solution URL here](https://your-solution-url.com)
@@ -61,8 +55,8 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - CSS Grid
 - Mobile-first workflow
 - [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
+- [Sass](https://sass-lang.com/) - CSS preprocessor
+- [Vite](https://vitejs.dev/) - JS tooling framework
 
 **Note: These are just examples. Delete this note and replace the list above with your own choices**
 
@@ -75,15 +69,17 @@ To see how you can add code snippets, see below:
 ```html
 <h1>Some HTML code I'm proud of</h1>
 ```
+
 ```css
 .proud-of-this-css {
   color: papayawhip;
 }
 ```
+
 ```js
 const proudOfThisFunc = () => {
-  console.log('🎉')
-}
+  console.log('🎉');
+};
 ```
 
 If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,92 @@
 function App() {
   return (
     <>
-      <div className="container">
-        <h1>multi task form</h1>
+      <div className="container container--full-center">
+        <div className="form-wrapper">
+          {/******************************************** Sidebar ********************************************/}
+          <div className="form-wrapper__steps-panel ">
+            <div className="form-wrapper__svg"></div>
+            <ul className="form-wrapper__steps-list steps-list flow">
+              <li className="steps-list__step">
+                <a href="#" className="steps-list__link">
+                  <span className="steps-list__step-text">
+                    <span className="steps-list__step-number">Step 1</span>
+                    <span className="steps-list__step-title">Your info</span>
+                  </span>
+                </a>
+              </li>
+              <li className="steps-list__step">
+                <a href="#" className="steps-list__link">
+                  <span className="steps-list__step-text">
+                    <span className="steps-list__step-number">Step 2</span>
+                    <span className="steps-list__step-title">Select plan</span>
+                  </span>
+                </a>
+              </li>
+              <li className="steps-list__step">
+                <a href="#" className="steps-list__link">
+                  <span className="steps-list__step-text">
+                    <span className="steps-list__step-number">Step 3</span>
+                    <span className="steps-list__step-title">Add-ons</span>
+                  </span>
+                </a>
+              </li>
+              <li className="steps-list__step">
+                <a href="#" className="steps-list__link">
+                  <span className="steps-list__step-text">
+                    <span className="steps-list__step-number">Step 4</span>
+                    <span className="steps-list__step-title">Summary</span>
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/******************************************** Form ********************************************/}
+          <form className="form form-wrapper__form">
+            <div className="form__inputs">
+              {/********************* Form step-1 *********************/}
+              <fieldset className="form__step-1 flow">
+                <legend className="form__step-header">
+                  <h2 className="form__title | p-heading">Personal info</h2>
+                  <p className="form__body">Please provide your name, email address, and phone number.</p>
+                </legend>
+                {/*  */}
+                <label className="form__labels" htmlFor="name-input">
+                  Name
+                  <span className="form__error-text" id="name-input-error">
+                    error text
+                  </span>
+                </label>
+                <input className="form__input" type="text" placeholder="e.g. Stephen King" name="name" id="name-input" aria-describedby="name-input-error" />
+                {/*  */}
+                <label className="form__labels" htmlFor="email-input">
+                  Email Address
+                  <span className="form__error-text" id="email-input-error">
+                    error text
+                  </span>
+                </label>
+                <input className="form__input" type="email" placeholder="e.g. stephenking@lorem.com" name="email" id="email-input" aria-describedby="email-input-error" />
+                {/*  */}
+                <label className="form__labels" htmlFor="number-input">
+                  Phone Number
+                  <span className="form__error-text" id="number-input-error">
+                    error text
+                  </span>
+                </label>
+                <input className="form__input" type="number" placeholder="e.g. +1 234 567 890" name="number" id="number-input" aria-describedby="number-input-error" />
+                {/*  */}
+              </fieldset>
+            </div>
+            <div className="form__cta-button-group">
+              <button className="form__cta-button cta-button cta-button--text">Go Back</button>
+              <button className="form__cta-button cta-button cta-button--contained">Next Step</button>
+            </div>
+          </form>
+        </div>
         <p style={{ display: 'none' }}>
-          {/* Sidebar start  */}
-          Step 1 Your info Step 2 Select plan Step 3 Add-ons Step 4 Summary
           {/* Sidebar end  */}
           {/* Step 1 start  */}
-          Personal info Please provide your name, email address, and phone number. Name e.g. Stephen King Email Address e.g. stephenking@lorem.com Phone Number e.g. +1 234 567 890 Next Step
+          Name Email Address Phone Number Next Step
           {/* Step 1 end  */}
           {/* Step 2 start  */}
           Select your plan You have the option of monthly or yearly billing. Arcade $9/mo Advanced $12/mo Pro $15/mo Monthly Yearly Go Back Next Step
