5975305142d1b90cc14a2648bed3a55f306ef6a1
hassaneljebyly <hassaneljeby@gmail.com>
2024-02-28 09:59:46 +0100

    finish styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Wrapper from './components/layout/Wrapper';
 import { FormContextProvider } from './context/FormContextProvider';
-
+// ToDo : transition animation
+// ToDo : edit README.md
 function App() {
   return (
     <main>
